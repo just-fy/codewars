c84a8c7743b812bb32fed5796137d3d3cb6c2eff
Nastya Faizulina <just-fy@mail.ru>
2019-01-15 19:18:28 +0300

    add task in comments

--- simpleSumOfPairs.js
+++ simpleSumOfPairs.js
@@ -1,3 +1,13 @@
+// Given an integer n, find two integers a and b such that:
+
+// A) a >= 0 and b >= 0
+// B) a + b = n
+// C) DigitSum(a) + Digitsum(b) is maximum of all possibilities.
+// You will return the digitSum(a) + digitsum(b).
+
+// For example:
+// solve(29) = 11. If we take 15 + 14 = 29 and digitSum = 1 + 5 + 1 + 4 = 11. There is no larger outcome.
+
 const digitSum = n => [...n.toString()].map(n => Number(n)).reduce((a, b) => a + b);
 const solve = n => {
   if (n < 0) {
